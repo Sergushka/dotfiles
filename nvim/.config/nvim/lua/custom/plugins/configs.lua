acf791829dd299710f3d4ec96ff650705eeb296a
Sergushka <39465148+Sergushka@users.noreply.github.com>
2023-06-13 00:10:26 +0300

    Modularize keymaps and configs, Add nvim-tree

---
+++ nvim/.config/nvim/lua/custom/plugins/configs.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+opt.nu = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+opt.termguicolors = true
+opt.colorcolumn = "80"
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+return {}
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,38 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.termguicolors = true
-vim.opt.colorcolumn = "80"
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("i", "<C-BS>", "<Esc>cvb", {})
-vim.keymap.set("i", "<C-e>", "<C-o>a")
-
 return {
     {
         'akinsho/toggleterm.nvim',
@@ -84,10 +52,23 @@ return {
         },
     },
     {
-        'rose-pine/neovim',
-        as = 'rose-pine',
+        'catppuccin/nvim',
         config = function()
-            vim.cmd.colorscheme 'rose-pine-moon'
+            vim.cmd.colorscheme 'catppuccin-mocha'
         end
     },
+    {
+        'tpope/vim-surround'
+    },
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+        config = function()
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+            require("nvim-tree").setup()
+        end
+    }
 }
