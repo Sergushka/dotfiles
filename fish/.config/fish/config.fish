7df81682d7df984f2d393bbfac32920fb6732793
Sergushka <39465148+Sergushka@users.noreply.github.com>
2023-06-19 01:58:18 +0300

    Add starfish prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,18 @@
+set fish_greeting
+
+alias vim='nvim'
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias commit='git commit -m'
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
