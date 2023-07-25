f030b5c46a62e6e85f6e96ec122db6da74f9c093
Sergushka <39465148+Sergushka@users.noreply.github.com>
2023-07-26 01:49:37 +0300

    Remove old configs

--- ohmyzshinstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
